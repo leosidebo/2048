69a3d0a046e5a4bd4358b4f2c12780f662ed3eb5
sniperkatten <sniperkatten@hotmail.com>
2020-12-22 16:57:07 +0100

    m
    test main, not final,
    game.c implementaions

--- game.c
+++ game.c
@@ -1,6 +1,8 @@
 #include "array.h"
 #include <stdlib.h>
-
+#include <time.h>
+#include <stdio.h>
+#include <stdbool.h>
 /* Global variables */
 
 //Global gameboard variable.
@@ -12,7 +14,7 @@ Array *board;
 static int random_num(int max, int min)
 {
     srand(time(NULL));
-    return = rand() & max + min;
+    return rand() & max + min;
 }
 
 /* External functions */
@@ -22,7 +24,27 @@ static int random_num(int max, int min)
 void game_new(void)
 {
     board = array_create(4,4);
-    array_set()
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            array_set(board,i,j,0);
+        }
+    }
+
+    int r_col = random_num(3,0);
+
+    array_set(board, random_num(3,0), random_num(3,0), 4);
+
+    if ( == 0) {
+        /* code */
+    }
+    array_set(board, random_num(3,0), random_num(3,0), 2);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+
+            printf("%d  ",game_get_square(i,j));
+        }
+        printf("\n");
+    }
 }
 
 // Quit the current game.
@@ -34,7 +56,7 @@ void game_quit(void)
 // Return the value of the specified piece (0 if empty).
 int game_get_square(int row, int column)
 {
-
+    return array_get(board, row, column);
 }
 
 // Slide all pieces up, right, down, or left.
